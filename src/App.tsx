1d3f722ff43f56fe31b1898bd6da61cd06c6da90
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-11-01 23:37:09 +0300

    fix: fixed roting issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { MainPage } from 'components/MainPage';
 import { WeatherDetails } from 'components/WeatherDetails';
 import React, { FC } from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App: FC = () => {
   return (
     <>
-      <nav>
-        <NavLink to={'/'}>Main</NavLink>
-        <NavLink to={'/details'}>WeatherDetails</NavLink>
-      </nav>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<Navigate to={'/weather/moscow'} />} />
+        <Route path="/weather/:city" element={<MainPage />} />
         <Route path="/details" element={<WeatherDetails />} />
       </Routes>
     </>
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,17 @@
 import React, { FC } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
 
 export const MainPage: FC = () => {
-  return <p>MainPage</p>;
+  const { city } = useParams();
+
+  return (
+    <>
+      <nav>
+        <NavLink to={'/weather/minsk'}>Minsk</NavLink>
+        <NavLink to={'/weather/moscow'}>Moscow</NavLink>
+        <NavLink to={'/weather/bratislava'}>Bratislava</NavLink>
+      </nav>
+      <h1>{city}</h1>
+    </>
+  );
 };
